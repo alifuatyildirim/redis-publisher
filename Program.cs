848a610edf6b2327c0171797f967f9d48882e61d
alifuatyildirim <alifuat.yildirim@hotmail.com>
2022-07-27 17:59:51 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿using StackExchange.Redis;
+
+const string RedisConnectionString = "localhost:6379";
+ConnectionMultiplexer connection=ConnectionMultiplexer.Connect(RedisConnectionString);
+const string Channel="channel_one";
+var pubsub=connection.GetSubscriber();
+pubsub.PublishAsync(Channel,"test alisssss",CommandFlags.FireAndForget);
+Console.WriteLine("Mesaj gönderildi.");
